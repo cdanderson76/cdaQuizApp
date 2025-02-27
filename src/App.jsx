b5af0b6cb2d09bce38387dccbdb3e91e7eceda34
Clifford Anderson <138369806+cdanderson76@users.noreply.github.com>
2025-02-26 22:35:40 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './index.css'
+import delta from './images/delta-center.jpg';
+
+export default function App() {
+
+  return (
+    <>
+      <div className='canvas'>
+        <Quiz />
+      </div>
+    </>
+  )
+}
+
+function Quiz() {
+  return (
+    <div className='container'>
+      <h1>Quiz App</h1>
+      <h3>(Stadiums and Arenas)</h3>
+      <hr />
+      <QuizItem />
+    </div>
+  )
+}
+
+function QuizItem() {
+  return (
+    <div className='question-container'>
+      <h2>What NBA basketball team host home games in this arena?</h2>
+      <img src={delta} alt="" />
+      <ul>
+        <li>Chicago Bulls</li>
+        <li>Utah Jazz</li>
+        <li>Oklahoma City Thunder</li>
+        <li>Phoenix Suns</li>
+      </ul>
+    </div>
+  )
+}
