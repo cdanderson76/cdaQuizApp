3244bb95ca8b7164fd1521b20d763b2006530084
Clifford <c.andersoninfotech@gmail.com>
2025-02-28 13:17:01 -0600

    modified and added more data to the data file and UI to show number of questions

--- src/data.js
+++ src/data.js
@@ -8,10 +8,12 @@ import littlecaesars from './images/littlecaesars.jpg';
 import lucas from './images/lucas.jpg';
 import lumen from './images/lumen.jpg';
 import paycom from './images/paycom.jpg';
+import trivia from './images/trivia.jpeg';
+import nfltriv from './images/nfltriv.jpg';
 
 export const data = [
     {
-        title: 'Stadiums and Arenas',
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NBA basketball team host home games in this arena?',
         facility: delta,
         option1: 'Chicago Bulls',
@@ -22,7 +24,29 @@ export const data = [
     },
 
     {
-        title: 'Stadiums and Arenas',
+        title: 'NBA Athletes',
+        question: 'What NBA basketball player is most famous for wearing #30?',
+        facility: trivia,
+        option1: 'Steph Curry',
+        option2: 'Reggie Miller',
+        option3: 'Seth Curry',
+        option4: 'Kevin Durant',
+        ans: 1
+    },
+
+    {
+        title: 'Teams, Stadiums, and Arenas',
+        question: 'What NFL team picked up in the MIDDLE OF THE NIGHT, and moved to Indianapolis, Indiana to become the Indianapolis Colts on March 28th, 1984?',
+        facility: nfltriv,
+        option1: 'Houston Oilers',
+        option2: 'New York Jets',
+        option3: 'Washington Redskins',
+        option4: 'Baltimore Ravens',
+        ans: 4
+    },
+
+    {
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NBA basketball team host home games in this arena?',
         facility: paycom,
         option1: 'Chicago Bulls',
@@ -33,7 +57,7 @@ export const data = [
     },
 
     {
-        title: 'Stadiums and Arenas',
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NFL football team host home games in this stadium?',
         facility: allegiant,
         option1: 'Dallas Cowboys',
@@ -44,7 +68,18 @@ export const data = [
     },
 
     {
-        title: 'Stadiums and Arenas',
+        title: 'NFL Athletes',
+        question: 'Which of the pair of siblings below are current NFL coaches (as of 2025)?',
+        facility: nfltriv,
+        option1: 'Travis Kelce and Jason Kelce',
+        option2: 'Jim Harbaugh and John Harbaugh',
+        option3: 'Stefon Diggs and Trevon Diggs',
+        option4: 'JJ Watt and TJ Watt',
+        ans: 2
+    },
+
+    {
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NBA basketball team host home games in this arena?',
         facility: fedex,
         option1: 'Memphis Grizzlies',
@@ -55,7 +90,7 @@ export const data = [
     },
 
     {
-        title: 'Stadiums and Arenas',
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NBA basketball team host home games in this arena?',
         facility: fiserv,
         option1: 'Phoenix Suns',
@@ -66,7 +101,18 @@ export const data = [
     },
 
     {
-        title: 'Stadiums and Arenas',
+        title: 'NBA Athletes',
+        question: `What NBA basketball player was known as 'Agent 0'?`,
+        facility: trivia,
+        option1: 'Steph Curry',
+        option2: 'Gilbert Arenas',
+        option3: 'Isiah Thomas',
+        option4: 'Allen Iverson',
+        ans: 2
+    },
+
+    {
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NFL football team host home games in this stadium?',
         facility: levi,
         option1: 'Cincinatti Bengals',
@@ -77,7 +123,7 @@ export const data = [
     },
 
     {
-        title: 'Stadiums and Arenas',
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NBA basketball team host home games in this arena?',
         facility: intuit,
         option1: 'Golden State Warriors',
@@ -88,7 +134,7 @@ export const data = [
     },
 
     {
-        title: 'Stadiums and Arenas',
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NFL football team host home games in this stadium?',
         facility: lumen,
         option1: 'Seattle Seahawks',
@@ -99,7 +145,7 @@ export const data = [
     },
 
     {
-        title: 'Stadiums and Arenas',
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NBA basketball team host home games in this arena?',
         facility: littlecaesars,
         option1: 'Detroit Pistons',
@@ -110,7 +156,18 @@ export const data = [
     },
 
     {
-        title: 'Stadiums and Arenas',
+        title: 'NBA Athletes',
+        question: 'What NBA basketball player was picked at #5 by the Dallas Mavericks in order to trade for Luka Doncic at #3 by the Atlanta Hawks?',
+        facility: trivia,
+        option1: 'Trae Young',
+        option2: 'Jaren Jackson Jr.',
+        option3: 'Marvin Bagley III',
+        option4: 'Collin Sexton',
+        ans: 1
+    },
+
+    {
+        title: 'Teams, Stadiums, and Arenas',
         question: 'What NFL football team host home games in this stadium?',
         facility: lucas,
         option1: 'Pittsburg Steelers',
